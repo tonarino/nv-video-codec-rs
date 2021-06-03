74617cc6b787d851bb882642b158c1225a13d796
Edward Yang <edward.yang6771@gmail.com>
2021-06-02 21:06:16 -0500

    Bind to sample encoder and decoder instead for now

--- nv-video-codec-sys/wrapper.h
+++ nv-video-codec-sys/wrapper.h
@@ -1,3 +1,7 @@
-#include <cuviddec.h>
-#include <nvcuvid.h>
-#include <nvEncodeAPI.h>
+// #include <cuviddec.h>
+// #include <nvcuvid.h>
+// #include <nvEncodeAPI.h>
+#include <NvDecoder.h>
+#include <NvEncoder.h>
+// #include <NvCodecUtils.h>
+// #include <ColorSpace.h> // bindgen does not like template functions
