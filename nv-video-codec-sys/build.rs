f8883d00c0d4aaf6ad379d9907e2a81c1ba46ed4
Brian Schwind <brianmschwind@gmail.com>
2021-06-11 14:59:51 +0900

    Export const declarations as well

--- nv-video-codec-sys/build.rs
+++ nv-video-codec-sys/build.rs
@@ -68,6 +68,7 @@ fn main() {
         .clang_arg(format!("-I{}", cuda_include.to_string_lossy()))
         .constified_enum_module("cudaVideoCodec_enum")
         .rustified_enum(".*")
+        .allowlist_var("(?i)(.*cu.*|.*nv.*)")
         .allowlist_type("(?i)(.*cu.*|.*nv.*)")
         .allowlist_function("(?i)(.*cu.*|.*nv.*)")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
