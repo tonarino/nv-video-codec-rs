89c50200fc2aef65f8805e347f7f586ae93d5830
Edward Yang <edward.yang6771@gmail.com>
2021-06-12 03:19:32 -0500

    Update cuda lib paths

--- scripts/actions/install_cuda_ubuntu.sh
+++ scripts/actions/install_cuda_ubuntu.sh
@@ -141,12 +141,9 @@ CUDA_PATH=/usr/local/cuda-${CUDA_MAJOR}.${CUDA_MINOR}
 echo "CUDA_PATH=${CUDA_PATH}"
 export CUDA_PATH=${CUDA_PATH}
 
-# setup symlink
-sudo ln -s "$CUDA_PATH/lib64/stubs/libcuda.so" "$CUDA_PATH/lib64/stubs/libcuda.so.1" 
-
 # Quick test. @temp
 export PATH="$CUDA_PATH/bin:$PATH"
-export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
 nvcc -V
 
 # If executed on github actions, make the appropriate echo statements to update the environment
@@ -155,5 +152,8 @@ if [[ $GITHUB_ACTIONS ]]; then
     echo "Adding CUDA to CUDA_PATH, PATH and LD_LIBRARY_PATH"
     echo "CUDA_PATH=${CUDA_PATH}" >> $GITHUB_ENV
     echo "${CUDA_PATH}/bin" >> $GITHUB_PATH
-    echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
+    echo "${CUDA_PATH}/lib64" >> $GITHUB_PATH
+    echo "${CUDA_PATH}/lib64/stubs" >> $GITHUB_PATH
+    echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib64:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
+    echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib64/stubs:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
 fi
