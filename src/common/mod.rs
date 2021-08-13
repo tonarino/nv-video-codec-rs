285aafbc3e73535b037df3e4e1169a85428a54f7
Edward Yang <edward.yang6771@gmail.com>
2021-08-13 16:59:50 -0500

    Move types out of common to decoder

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -2,11 +2,5 @@
 mod macros;
 
 pub mod cuda_result;
-pub mod types;
 
 pub use cuda_result::*;
-pub use types::{
-    ChromaFormat, ChromaFormatConvertError, Codec, CodecConvertError, CreateFlags,
-    CreateFlagsConvertError, DeinterlaceMode, DeinterlaceModeConvertError, Dim, Rect,
-    SurfaceFormat, SurfaceFormatConvertError,
-};
