1583aa078cb3732c4f8672555652bc5edad97fa3
Marek Bernat <mbernat@users.noreply.github.com>
2026-02-09 09:59:40 +0100

    Expect lints

--- nv-video-codec-sys/src/lib.rs
+++ nv-video-codec-sys/src/lib.rs
@@ -1,21 +1,14 @@
-// TODO: check these attributes are still needed
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(clippy::unreadable_literal)]
-#![allow(clippy::redundant_static_lifetimes)]
-#![allow(clippy::too_many_arguments)]
-// broken links from bindgen
-#![allow(rustdoc::broken_intra_doc_links)]
-// https://github.com/rust-lang/rust-bindgen/issues/1651
-#![allow(deref_nullptr)]
+#![expect(non_upper_case_globals)]
+#![expect(non_camel_case_types)]
+#![expect(non_snake_case)]
+#![expect(clippy::too_many_arguments)]
 // https://github.com/rust-lang/rust-bindgen/issues/2807
-#![allow(unnecessary_transmutes)]
+#![expect(unnecessary_transmutes)]
 // https://github.com/rust-lang/rust-bindgen/issues/2807
-#![allow(clippy::useless_transmute)]
-#![allow(clippy::missing_safety_doc)]
+#![expect(clippy::useless_transmute)]
+#![expect(clippy::missing_safety_doc)]
 // https://github.com/rust-lang/rust-bindgen/issues/3053
-#![allow(clippy::ptr_offset_with_cast)]
+#![expect(clippy::ptr_offset_with_cast)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
