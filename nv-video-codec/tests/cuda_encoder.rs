7431e6726836fa0a1e1a276209fc1a9a634d7203
Marek Bernat <mbernat@users.noreply.github.com>
2026-03-23 17:23:46 +0100

    Add a failing cuda_encoder test

---
+++ nv-video-codec/tests/cuda_encoder.rs
@@ -0,0 +1,174 @@
+extern crate anyhow;
+extern crate gl;
+extern crate log;
+extern crate simple_logger;
+
+#[path = "utils.rs"]
+#[macro_use]
+mod utils;
+
+use std::{
+    io::Write,
+    time::{Duration, Instant},
+};
+
+use anyhow::Result;
+use nv_video_codec::encoder::{
+    nvencodercuda::NvEncoderCuda, types::BufferFormat, NvEncoderExt, NvEncoderSettings,
+};
+use nv_video_codec_sys::{
+    guids, _NV_ENC_PIC_FLAGS, NV_ENC_PARAMS_RC_MODE, NV_ENC_PIC_PARAMS, NV_ENC_TUNING_INFO,
+};
+use rustacuda::{
+    device::Device,
+    prelude::{Context, ContextFlags},
+};
+use simple_logger::SimpleLogger;
+
+fn init_cuda_ctx() -> Result<Context> {
+    rustacuda::init(rustacuda::CudaFlags::empty())?;
+    let device = Device::get_device(0)?;
+    let context =
+        Context::create_and_push(ContextFlags::MAP_HOST | ContextFlags::SCHED_AUTO, device)?;
+    Ok(context)
+}
+
+fn util_init_encoder(width: u32, height: u32, format: BufferFormat) -> Result<NvEncoderCuda> {
+    let context = init_cuda_ctx()?;
+    let settings = NvEncoderSettings::new(width, height, format);
+
+    let encoder = NvEncoderCuda::new(context, settings).expect("Could not create NvEncoderCuda");
+    Ok(encoder)
+}
+
+fn util_create_encoder(encoder: &mut NvEncoderCuda) -> Result<()> {
+    let mut params = encoder.create_default_encoder_params(
+        guids::NV_ENC_CODEC_HEVC_GUID,
+        // preset guid seems to have no real effect on the speed???
+        // needs testing as well
+        guids::NV_ENC_PRESET_P3_GUID,
+        // can't really see a difference between ULTRA_LOW_LATENCY and LOW_LATENCY???
+        // ULTRA_LOW might be like 0.5ms faster at times?
+        // needs testing on dev installation
+        NV_ENC_TUNING_INFO::NV_ENC_TUNING_INFO_ULTRA_LOW_LATENCY,
+    )?;
+    params.frameRateNum = 60;
+    unsafe {
+        (*params.encodeConfig).rcParams.rateControlMode =
+            NV_ENC_PARAMS_RC_MODE::NV_ENC_PARAMS_RC_CBR;
+        // (*params.encodeConfig).rcParams.multiPass =
+        //     NV_ENC_MULTI_PASS::NV_ENC_TWO_PASS_QUARTER_RESOLUTION;
+        (*params.encodeConfig).rcParams.lowDelayKeyFrameScale = 1;
+        (*params.encodeConfig).rcParams.averageBitRate = 13 * 1000 * 1000;
+        (*params.encodeConfig).rcParams.vbvBufferSize = encoder.get_frame_size()?;
+        (*params.encodeConfig).rcParams.vbvInitialDelay = encoder.get_frame_size()?;
+        (*params.encodeConfig).rcParams.set_enableAQ(1);
+        // required for use with ffmpeg, not with nvcodec
+        (*params.encodeConfig).encodeCodecConfig.hevcConfig.set_repeatSPSPPS(1);
+    }
+    encoder.create_encoder(&params)?;
+    Ok(())
+}
+
+#[test]
+fn init_encoder() -> Result<()> {
+    let _encoder = util_init_encoder(1280, 720, BufferFormat::NV12)?;
+
+    Ok(())
+}
+
+#[test]
+fn create_encoder() -> Result<()> {
+    let mut encoder = util_init_encoder(1280, 720, BufferFormat::NV12)?;
+    util_create_encoder(&mut encoder)?;
+
+    Ok(())
+}
+
+#[test]
+fn encode_single_frame_grayscale() -> Result<()> {
+    let (width, height) = (1280, 720);
+    let mut encoder = util_init_encoder(width, height, BufferFormat::NV12)?;
+    util_create_encoder(&mut encoder)?;
+
+    let data = include_bytes!("../resources/test/decode_out_grayscale.nv12");
+    assert_eq!(data.len(), encoder.get_frame_size()? as usize);
+
+    let _resource = encoder.get_next_input_resource();
+
+    // TODO: copy data to resource
+
+    let mut packet = Vec::new();
+    encoder.encode_frame(&mut packet, None)?;
+
+    let mut f = std::fs::File::create("encode_out_grayscale.hevc")?;
+    for frame in &packet {
+        f.write_all(frame)?;
+    }
+
+    encoder.end_encode(&mut packet)?;
+    assert_eq!(0, packet.len());
+
+    Ok(())
+}
+
+#[test]
+fn encode_multi_frame_3k() -> Result<()> {
+    let _ = SimpleLogger::new().init();
+    let (width, height) = (3088, 2076);
+    let mut encoder = util_init_encoder(width, height, BufferFormat::NV12)?;
+    util_create_encoder(&mut encoder)?;
+
+    let data = include_bytes!("../resources/test/decode_out_3k.nv12");
+    assert_eq!(data.len(), encoder.get_frame_size()? as usize);
+
+    let mut f = std::fs::File::create("encode_out_3k.hevc")?;
+    let mut packet = Vec::new();
+
+    const NUM_TORTURE_FRAMES: usize = 500;
+    let mut total_time = Duration::from_millis(0);
+    let mut blocked_time = Duration::from_millis(0);
+    let mut frames_encoded = 0;
+    for _ in 0..NUM_TORTURE_FRAMES {
+        let start_time = Instant::now();
+        let params = NV_ENC_PIC_PARAMS {
+            // force intra-frame and force per-frame metadata
+            encodePicFlags: _NV_ENC_PIC_FLAGS::NV_ENC_PIC_FLAG_FORCEIDR.0
+                | _NV_ENC_PIC_FLAGS::NV_ENC_PIC_FLAG_OUTPUT_SPSPPS.0,
+            ..Default::default()
+        };
+        encoder.encode_frame_from_data(data, width, height, Some(params), &mut packet)?;
+
+        frames_encoded += 1;
+        total_time += start_time.elapsed();
+        blocked_time += start_time.elapsed();
+        if frames_encoded % 500 == 0 {
+            info_ctx!(
+                "encode_multi",
+                "Encoded last 500 frames in {:?}, {:?} per frame",
+                blocked_time,
+                blocked_time / 500
+            );
+            blocked_time = Duration::from_millis(0);
+        }
+        while start_time.elapsed() < Duration::from_millis(1000) / 60 {
+            std::thread::sleep(Duration::from_micros(10));
+        }
+    }
+    info_ctx!(
+        "encode_multi",
+        "Encoded {} frames in {:?}, {:?} per frame",
+        NUM_TORTURE_FRAMES,
+        total_time,
+        total_time / NUM_TORTURE_FRAMES as u32
+    );
+
+    for frame in &packet {
+        f.write_all(frame)?;
+    }
+
+    encoder.end_encode(&mut packet)?;
+    assert_eq!(0, packet.len());
+
+    Ok(())
+}
